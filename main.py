ff8f9155a6e6d55e9b4221ffd9e7a89af15b1207
lara <larafercue@gmail.com>
2018-12-15 20:55:23 +0100

    Using the new match 3 board

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import pathlib
+
 import pygame
 
-from src import window, board
+from src import window, game_object, match_three_board
 
-WIN_WIDTH = 800
-WIN_HEIGHT = 900
+WIN_WIDTH = 1000
+WIN_HEIGHT = 1000
 
 window = window.Window(width=WIN_WIDTH, height=WIN_HEIGHT, title="Werewolves versus Vampires")
-board = board.Board((0, 100), (WIN_WIDTH, WIN_HEIGHT - 100))
+window.color = pygame.Color('yellow')
+board_decorator: game_object.GameObject = game_object.GameObject((150, 300), (WIN_WIDTH - 300, WIN_HEIGHT - 300))
+board_decorator.sprite = pathlib.Path('resources/board_decorator.png'), game_object.Size(700, 700)
 
-board.color = pygame.Color('red')
-board.divide_board(160, 160, [pygame.Color('green'), pygame.Color('blue')])
-window.objects.append(board)
+board = match_three_board.MatchThreeBoard((50, 50), (WIN_WIDTH - 400, WIN_HEIGHT - 400), 10, 10)
+board.color = (0, 255, 255)
+board.init_board(55, 55)
+
+board_decorator.children.append(board)
+window.objects.append(board_decorator)
 while True:
     window.process_events()
     window.draw_window()
