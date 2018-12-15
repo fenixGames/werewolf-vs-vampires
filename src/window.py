d893f7bbf06a1b98364e98e6210daa2286c3b4c5
lara <larafercue@gmail.com>
2018-12-15 15:32:47 +0100

    Adding a way to add/remove objects from a window

--- src/window.py
+++ src/window.py
@@ -1,13 +1,16 @@
 import sys
-from typing import Tuple
+from typing import Tuple, List
 
 import pygame
+
+from src.game_object import GameObject
 
 
 class Window:
     def __init__(self, width: int, height: int, title: str):
         self.__width: int = width
         self.__height: int = height
+        self.__objects: List[GameObject] = []
 
         pygame.init()
         self.__screen: pygame.Surface = pygame.display.set_mode(self.size)
@@ -25,8 +28,15 @@ class Window:
     def size(self) -> Tuple[int, int]:
         return self.width, self.height
 
-    @staticmethod
-    def draw_window():
+    @property
+    def objects(self) -> List[GameObject]:
+        return self.__objects
+
+    def draw_window(self):
+        for obj in self.__objects:
+            surface = obj.draw()
+            if isinstance(surface, pygame.Surface):
+                self.__screen.blit(surface, obj.position.to_tuple())
         pygame.display.flip()
 
     @staticmethod
