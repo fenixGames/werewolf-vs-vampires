cb5b2beb130136abca8c2cd47fd7a95e2baf8a67
lara <larafercue@gmail.com>
2018-12-15 18:28:46 +0100

    Removing duplicated code for the sprite

--- src/board.py
+++ src/board.py
@@ -1,4 +1,3 @@
-import pathlib
 from typing import Tuple, List
 
 import pygame
@@ -9,17 +8,6 @@ from src.game_object import GameObject
 class Board(GameObject):
     def __init__(self, position: Tuple[int, int], size: Tuple[int, int]):
         super().__init__(position, size)
-        self.__background: pygame.Surface = None
-
-    @property
-    def background(self) -> pygame.Surface:
-        return self.__background
-
-    @background.setter
-    def background(self, path_to_image: pathlib.Path):
-        if not path_to_image.absolute().is_file():
-            raise FileNotFoundError(f'The image for the board {path_to_image.absolute().as_uri()} does not exist')
-        self.__background = pygame.image.load(path_to_image.absolute().as_posix()).convert()
 
     def divide_board(self, square_width: int, square_height: int, colors: List[pygame.Color]):
         """ Divides a board equally using a tile width and height
@@ -60,14 +48,3 @@ class Board(GameObject):
 class Square(GameObject):
     def __init__(self, position: Tuple[int, int], size: Tuple[int, int]):
         super().__init__(position, size)
-        self.__sprite: pygame.Surface = None
-
-    @property
-    def sprite(self) -> pygame.Surface:
-        return self.__sprite
-
-    @sprite.setter
-    def sprite(self, path_to_file: pathlib.Path):
-        if not path_to_file.absolute().is_file():
-            raise FileNotFoundError(f'The sprite for the square {path_to_file.absolute().as_uri()} does not exist')
-        self.__sprite = pygame.image.load(path_to_file.absolute().as_posix()).convert()
