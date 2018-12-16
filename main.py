7e31812c21512128813ce41bb249d83567f9b297
lara <larafercue@gmail.com>
2018-12-16 11:28:35 +0100

    Adjusting the width to the new board decorator

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ import pathlib
 import pygame
 
 import lib.vector
-from src import board
 from lib import events, game_object, graphics, window
+from src import board
 
-WIN_WIDTH = 1000
+WIN_WIDTH = 700
 WIN_HEIGHT = 1000
 
 window = window.Window(width=WIN_WIDTH, height=WIN_HEIGHT, title="Werewolves versus Vampires")
 window.color = pygame.Color('yellow')
-board_decorator: game_object.GameObject = game_object.GameObject((150, 300), (WIN_WIDTH - 300, WIN_HEIGHT - 300))
+board_decorator: game_object.GameObject = game_object.GameObject((0, 300), (WIN_WIDTH, WIN_HEIGHT - 300))
 board_decorator.sprite = graphics.GraphicResource(pathlib.Path('resources/board_decorator.png'),
                                                   lib.vector.Size(700, 700)), lib.vector.Size(700, 700)
 
-board = board.MatchThreeBoard((75, 75), (WIN_WIDTH - 450, WIN_HEIGHT - 450), 10, 10)
-board.color = (0, 255, 255)
+board = board.MatchThreeBoard((75, 75), (WIN_WIDTH - 150, WIN_HEIGHT - 450), 10, 10)
+# board.color = (0, 255, 255)
 board.init_board(55, 55)
 
 board_decorator.children.append(board)
